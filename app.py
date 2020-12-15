3dd7b569ab5b6314bd544b25c355204fab80db20
alexv <a.karhunen53@gmail.com>
2020-12-15 20:32:02 +0300

    v1

---
+++ app.py
@@ -0,0 +1,17 @@
+import time
+
+
+slide1 = '<(^_^)>'
+slide2 = '<(^_~)>'
+slide3 = '<(^_^)>'
+slide4 = '<(о_О)>'
+
+time.sleep(1)
+print(slide1, end='\r')
+time.sleep(1)
+print(slide2, end='\r')
+time.sleep(1)
+print(slide3, end='\r')
+time.sleep(1)
+print(slide4, end='\r')
+time.sleep(1)
